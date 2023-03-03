41a8392f235535721d505b6c3775ef3a40874fc6
Nikita Sherbakov <nikita@sherbakov.dev>
2023-03-03 22:32:54 +0200

    feat: :sparkles: Add Vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import "styles/global.css";
 
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 
+import { Analytics } from "@vercel/analytics/react";
 import type { AppProps } from "next/app";
 
 const theme = extendTheme({
@@ -16,6 +17,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
       <Component {...pageProps} />
+      <Analytics />
     </ChakraProvider>
   );
 }
